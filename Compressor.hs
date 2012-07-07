c66d6e78c3bae8fca444bd11407c14f975a20aa9
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2012-07-07 18:11:59 +0200

    version

--- Compressor.hs
+++ Compressor.hs
@@ -21,8 +21,8 @@ main = do
     args <- getArgs
     ss <- collect args
     forM ( map dp ss ) $ \ s -> do
-        print $ pretty s 
-        print $ pretty ( compress supply s)
+        -- print $ pretty s 
+        -- print $ pretty ( compress supply s)
         print $ judge s
 
 
