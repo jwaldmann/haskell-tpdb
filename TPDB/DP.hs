90a5596e6a0396204ea3fd9e78d9c807ba96a44b
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2014-04-24 12:38:42 +0200

    SCC decomposition

--- TPDB/DP.hs
+++ TPDB/DP.hs
@@ -1,5 +1,7 @@
-module TPDB.DP ( dp )
+module TPDB.DP ( dp, components )
 
 where
 
 import TPDB.DP.Transform
+import TPDB.DP.Graph
+
--- TPDB/DP/Graph.hs
+++ TPDB/DP/Graph.hs
@@ -10,6 +10,8 @@ import TPDB.Plain.Read -- for testing
 import TPDB.Plain.Write -- for testing
 
 import qualified Data.Set as S
+import qualified Data.Map as M
+import Data.Graph ( stronglyConnComp, SCC(..) )
 import Control.Monad ( guard, forM )
 import Control.Applicative
 
@@ -25,15 +27,33 @@ walk defined t = case t of
         -> Node f <$> forM args ( walk defined )
     _ -> do i <- get ; put $ succ i ; return $ Var i
 
+-- | DP problems for strongly connected components, 
+-- topologically sorted
+components s = do 
+    let es = M.fromListWith (++) 
+           $ do (p,q) <- edges s ; return (p, [q])
+        key = M.fromList 
+            $ zip (filter strict $ rules s) [0.. ]
+    CyclicSCC vs <- stronglyConnComp $ do
+        (p, qs) <- M.toList es
+        return (p, key M.! p, map (key M.!) qs )
+    return $ s { rules = vs 
+                 ++ filter (not . strict) (rules s) }
+
+
+
+-- | edges of the estimated dependency graph
 edges s = do
     let def = S.filter isOriginal $ defined s
     u <- filter strict $ rules s
     v <- filter strict $ rules s
     guard $ unifies ( vmap Left $ rename_cap def $ rhs u ) 
-                    ( vmap Right $ rhs v )
+                    ( vmap Right $ lhs v )
     return (u,v)
 
-check = edges $ dp trs0
+check = edges $ dp sys
 
-Right trs0 = 
-    TPDB.Plain.Read.trs "(VAR x y) (RULES f(S(x),y)->S(f(x,y)))" 
+-- example from "DP Revisited" http://colo6-c703.uibk.ac.at/ttt/rta04.pdf
+Right sys = 
+    TPDB.Plain.Read.trs "(VAR x y) (RULES not(not(x)) -> x not(or(x,y)) -> and(not(x),not(y)) not(and(x,y)) -> or (not(x),not(y)) and(x,or(y,z)) -> or(and(x,z),and(y,z)) and(or(y,z),x) -> or(and(x,y),and(x,z)))"
+
